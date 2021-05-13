7e1701e9748dc01362c94b755381fdc71bf96abe
sushantGitH <sushantpoojary36@gmail.com>
2021-05-13 17:11:54 +0530

    firemode added

--- src/rooms/schema/mapManager/MapController.js
+++ src/rooms/schema/mapManager/MapController.js
@@ -3,58 +3,72 @@ const GameMap = require('./GameMap').GameMap;
 
 
 class MapController extends schema.Schema {
-    constructor() {
-        super();
-        this.timer = 0;
-        this.timerActive = false;
-        this.timerCount = 0;
-        this.fireTimeInSec = 4;
+  constructor() {
+    super();
+    this.leftTimer = 0;
+    this.leftTimerCoolDown = 7;
+    this.leftTimerActivateTimer = 10;
+    this.rightTimer = 0;
+    this.rightTimerCoolDown = 7;
+    this.rightTimerActivateTimer = 12;
 
-        /* this.timer = setTimeout(() => {
+    /* this.timer = setTimeout(() => {
 
-        }, this.fireTimeInSec * 1000); */
+    }, this.fireTimeInSec * 1000); */
 
+  }
+
+  /**
+   * @param {GameMap} map - Assign active GameMap
+   */
+  setMap(map) {
+    this.map = map;
+
+  }
+
+  initMap() {
+    if (this.map.name == 'fireMode') {
+      if (this.map.mode.get('leftEnabled')) {
+        this.enableLeftFire();
+      }
+      if (this.map.mode.get('rightEnabled')) {
+        this.enableRightFire();
+      }
     }
+  }
 
-    /**
-     * @param {GameMap} map - Assign active GameMap
-     */
-    setMap(map) {
-        this.map = map;
+  enableLeftFire() {
+    this.leftFireActivate = true;
+    this.leftTimer = setTimeout(() => {
+      this.disableLeftFire();
+    }, this.leftTimerCoolDown * 1000);
+  }
+  disableLeftFire() {
+    this.leftFireActivate = false;
+    this.leftTimer = setTimeout(() => {
+      this.enableLeftFire();
+    }, this.leftTimerActivateTimer * 1000);
+  }
 
-    }
-
-    initMap() {
-        if (this.map.name == 'fireMode') {
-            if (this.map.mode.get('leftEnabled')) {
-                this.enableLeftFire();
-            }
-        }
-    }
-
-    enableLeftFire() {
-        // this.leftFireActivate = true;
-        this.timer = setTimeout(() => {
-            // if (this.timerActive)
-            //     this.disableLeftFire();
-
-            this.fireTimeInSec++;
-        }, 1000);
-    }
-
-    disableLeftFire() {
-        //clearTimeout(this.timer);
-        this.timerActive = false;
-        // this.leftFireActivate = false;
-    }
-
+  enableRightFire() {
+    this.rightFireActivate = true;
+    this.rightTimer = setTimeout(() => {
+      this.disableRightFire();
+    }, this.rightTimerCoolDown * 1000);
+  }
+  disableRightFire() {
+    this.rightFireActivate = false;
+    this.rightTimer = setTimeout(() => {
+      this.enableRightFire();
+    }, this.rightTimerActivateTimer * 1000);
+  }
 
 
 }
 
 schema.defineTypes(MapController, {
-    map: GameMap,
-    fireTimeInSec: 'number',
-    leftFireActivate: 'boolean'
+  map: GameMap,
+  leftFireActivate: 'boolean',
+  rightFireActivate: 'boolean'
 });
 exports.MapController = MapController;
